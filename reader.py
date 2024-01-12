aca1adc1fe94386783930545388addfeaf75b860
python-nerd-3 <117922845+python-nerd-3@users.noreply.github.com>
2024-01-11 20:38:43 -0500

    added tag and item reading

--- reader.py
+++ reader.py
@@ -22,9 +22,24 @@ for i in r["blocks"]:
         block = i["block"]
         if block in {"func", "process"}: # 2nd time i've used a set in any language
             btype = i["data"]
+        elif block == "else":
+            print(f"\n{ind} else", end="")
         else:
             btype = i["action"]
-            print(f"\n{ind}{block}: {btype}", end=" ")
+            allItems = i["args"]["items"]
+            items = list(filter((lambda j: j["item"]["id"] != "bl_tag"), allItems))
+            tags = list(filter((lambda j: j["item"]["id"] == "bl_tag"), allItems))
+            items.sort(key=(lambda j: j["slot"]))
+            tags.sort(key=(lambda j: j["slot"]))
+            # below is the longest lambda known to man
+            itemDisps = list(map((lambda j: j["item"]["id"] + " | " +  (j["item"]["data"]["name"] if j["item"]["id"] != "item" else j["item"]["data"]["item"][(j["item"]["data"]["item"].find("id:") + 3):j["item"]["data"]["item"].find(",tag")])), items))
+            tagDisps = list(map(lambda j: j["item"]["data"]["tag"] + ": " + j["item"]["data"]["option"], tags))
+            itemDisps = str(itemDisps).replace("'", "")
+            if len(tagDisps) == 0:
+                tagDisps = ""
+            else: 
+                tagDisps = str(tagDisps).replace("'", "")
+            print(f"\n{ind}{block}: {btype} {itemDisps} {tagDisps}", end=" ")
     else:
         if i["direct"] == "open":
             print(" {", end=" ")
